cfc312913c5c486ca0c5df36033285b6863c2304
subash9860 <subaazkc@gmail.com>
2021-04-06 22:09:38 +0545

    Set Opration

--- q1SetOperation.c
+++ q1SetOperation.c
@@ -2,47 +2,9 @@
 Programs to implement set operations union, intersection, difference, and Cartesian product. 
 */
 #include<stdio.h>
-void unionSet(int a[10], int b[10], int setA, int setB);
-void intersectionSet(int c[10], int d[10], int setAa, int setBb);
-void AdiffB(int a[10], int b[10], int n, int m);
-void BdiffA(int a[10], int b[10], int n, int m);
-void cartesian(int a[10], int b[10],int n,int m);
-int c[10], i, j;
-int main()
-{
-	int a[10],b[10], n, m;
-	printf("How many elements in set A:\n");
-	scanf("%d",&n);
-	printf("Enter the elements for set A:\n");
-	for(i=0;i<n;i++)
-	{
-		scanf("%d",&a[i]);
-	}
-	printf("How many elements in set B:\n");
-	scanf("%d",&m);
-	printf("Enter  the elements for set B:\n");
-	for(j=0;j<m;j++)
-	{
-		scanf("%d",&b[j]);
-		
-	}
-	printf("\nset A:{");
-	for(i=0;i<n;i++)
-	printf("%d"",",a[i]);
-	printf("}\n");
-	
-	printf("set B:{");
-	for(j=0;j<m;j++)
-	printf("%d"",",b[j]);
-	printf("}\n");
-	unionSet(a,b,n,m);
-	intersectionSet(a,b,n,m);
-	AdiffB(a,  b,  n,m);
-	BdiffA(a, b, n,m);
-	cartesian(a, b,n,m);
-}
+int i,j, c[10];
 void unionSet(int g[10], int h[10], int sA, int sB)
-{    //The function to calculate union of two sets
+{
 	int k=0, x=0, f1=0 ;
 	for(i=0;i<sA;i++)
 	{
@@ -71,11 +33,10 @@ void unionSet(int g[10], int h[10], int sA, int sB)
 	}
 	printf("\n union of A and B is:\t{");
 	for(i=0;i<k;i++)
-	{
-	    printf("%d,",c[i]);
-	}
+	printf("%d ",c[i]);
 	printf("}\n");
 }
+
 void intersectionSet(int a[10], int b[10], int n, int m)
 {
 	int k=0;
@@ -90,13 +51,14 @@ void intersectionSet(int a[10], int b[10], int n, int m)
 			}
 		}
 	}
-	printf("intersection of A and B:\t{");
+	printf("intersection of A and B: {");
 	for(i=0;i<k;i++)
 	{
-		printf("%d"",",c[i]);
+		printf("%d ",c[i]);
 	}
 	printf("}\n");
 }
+
 void AdiffB(int a[10], int b[10], int n, int m)
 {
 	int k=0, f1=0;
@@ -121,9 +83,10 @@ void AdiffB(int a[10], int b[10], int n, int m)
 	}
 	printf("\n difference of A-B is:\t{");
 	for(i=0;i<k;i++)
-	printf("%d"",",c[i]);
+	printf("%d ",c[i]);
 	printf("}\n");
 }
+
 void BdiffA(int a[10], int b[10], int n, int m)
 {
 	int k=0, f1 =0;
@@ -148,24 +111,51 @@ void BdiffA(int a[10], int b[10], int n, int m)
 	}
 	printf("\n difference of B-A is:\t{");
 	for(i=0;i<k;i++)
-	printf("%d"",",c[i]);
+	printf("%d ",c[i]);
 	printf("}\n");
 }
+
 void cartesian(int a[10], int b[10],int n,int m)
 {
-	printf("\ncartesian product:");
-	printf("\n{");
+	printf("\ncartesian product:\n{");
 	for(i=0;i<n;i++)
 	{
 		for(j=0;j<m;j++)
-		{    
-			printf("(");
-			printf("%d,%d",a[i],b[j]);
-			printf(",");
-			printf(")");
-			printf(",");
-		}
+		printf("(%d,%d) ",a[i],b[j]);
 		printf("}");
 	}
 	printf("\n");
+}
+
+int main()
+{
+	int a[10],b[10], x, y;
+
+	printf("How many elements in set A:\n");
+	scanf("%d",&x);
+	printf("Enter the elements for set A:\n");
+	for(i=0;i<x;i++)
+	scanf("%d",&a[i]);
+
+	printf("How many elements in set B:\n");
+	scanf("%d", &y);
+	printf("Enter  the elements for set B:\n");
+	for(j=0;j<y;j++)
+	scanf("%d",&b[j]);
+
+	printf("\nset A:{");
+	for(i=0;i<x;i++)
+	printf("%d ",a[i]);
+	printf("}\n");
+	
+	printf("set B:{");
+	for(j=0;j<y;j++)
+	printf("%d ",b[j]);
+	printf("}\n");
+
+	unionSet(a,b,x,y);
+	intersectionSet(a,b,x,y);
+	AdiffB(a,  b,  x,y);
+	BdiffA(a, b, x,y);
+	cartesian(a, b,x,y);
 }
