9719eba1fbd6ef6116527902de080f69107c6e2a
subash9860 <subaazkc@gmail.com>
2021-04-06 22:11:09 +0545

    Floor and ceiling

--- q2CeilingNFloor.c
+++ q2CeilingNFloor.c
@@ -4,10 +4,11 @@
 #include<math.h>
 int main()
 {
-	float val;
-	printf("enter a float number:");
-	scanf("%f",&val);
-	printf("ceiling value of %f is %f\n",val,ceil(val));
-    printf("floor value of %f is %f\n",val,floor(val));	
+	float a, b, val;
+	printf("Enter  two number:\n");
+	scanf("%f%f",&a,&b);
+	val = a/b;
+	printf("ceiling value of %0.1f/%0.1f is:\t%0.0f\n",a,b,ceil(val));
+    printf("floor value of %0.1f/%0.1f is :\t%0.0f\n",a,b,floor(val));
     return 0;
 }
